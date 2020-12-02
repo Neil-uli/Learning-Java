2006d46b21aa395276306a6e02e240ca665955d5
erick12art@gmail.com <erick12art@gmail.com>
2020-12-01 23:24:09 -0600

    utilizando Long

--- 02.java
+++ 02.java
@@ -1,35 +1,60 @@
 import java.util.Scanner;
+
 class calculadora {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		Integer a = 0;
 		Integer b = 0;
 		Scanner entradaDeTeclado = new Scanner(System.in);
-		
+
 		// Suma
 		System.out.println("Ingresa a:");
 		a = entradaDeTeclado.nextInt();
 		System.out.println("ingresa b");
 		b = entradaDeTeclado.nextInt();
 		System.out.println("Resultado de a + b = " + suma(a, b));
-		
-		//Resta
+
+		// Resta
 		System.out.println("Ingresa a:");
 		a = entradaDeTeclado.nextInt();
 		System.out.println("ingresa b");
 		b = entradaDeTeclado.nextInt();
-		System.out.println("Resultado de a - b = " + resta(a,b));
-	}
+		System.out.println("Resultado de a - b = " + resta(a, b));
 
+		// Multiplicacion
+		System.out.println("Ingresa a:");
+		Long ab = Long.valueOf(entradaDeTeclado.nextLong());
+		System.out.println("ingresa b");
+		Long bc = Long.valueOf(entradaDeTeclado.nextLong());
+		System.out.println("Resultado de ab * bc = " + multiplicacion(ab, bc));
+
+		// Division
+		//System.out.println("Ingresa a:");
+		//a = entradaDeTeclado.nextLong();
+		//System.out.println("ingresa b");
+		//b = entradaDeTeclado.nextLong();
+		//System.out.println("Resultado de a / b = " + division(a, b));
+
+	}
 
 	public static int suma(int a, int b) {
 		Integer sum = 0;
-		sum = a+b;
+		sum = a + b;
 		return sum;
 	}
 
-	private static long resta(int a,int b ){
-	Integer resta = 0;
-	resta = a-b;
-	return resta;
+	private static long resta(int a, int b) {
+		Integer resta = 0;
+		resta = a - b;
+		return resta;
 	}
+
+	private static long multiplicacion(Long a, Long b) {
+		Long multiplicacion = a * b;
+		return multiplicacion;
+	}
+
+	//private static long division(long a,long b ){
+	//		Integer division = a/b;
+	//		return division;
+//}
 }
