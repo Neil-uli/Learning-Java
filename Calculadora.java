c1d15993ebc5bf836a435035444a5095414917be
ulises <ulisescamps9@gmail.com>
2021-04-13 16:02:09 -0500

    refactoring

--- Calculadora.java
+++ Calculadora.java
@@ -1,75 +1,75 @@
 import java.util.Scanner;
 
 class Calculadora {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Scanner reader = new Scanner(System.in);
-		System.out.println("Ingresa la operacion : \n 1 - Sumar\n 2- Restar\n 3-Multi\n4- Dividir");
-		int operacion = reader.nextInt();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Ingresa la operacion : \n 1 - Sumar\n 2- Restar\n 3-Multi\n4- Dividir");
+        int operacion = reader.nextInt();
 
-		int a, b;
+        int a, b;
 
-		switch (operacion) {
-			case 1:
-				System.out.println("Ingresa a:");
-				a = reader.nextInt();
-				System.out.println("ingresa b");
-				b = reader.nextInt();
-				suma(a, b);
-				break;
+        switch (operacion) {
+            case 1:
+                System.out.println("Ingresa a:");
+                a = reader.nextInt();
+                System.out.println("ingresa b");
+                b = reader.nextInt();
+                suma(a, b);
+                break;
 
-			case 2:
-				System.out.println("Ingresa a:");
-				a = reader.nextInt();
-				System.out.println("ingresa b");
-				b = reader.nextInt();
-				resta(a, b);
-				break;
+            case 2:
+                System.out.println("Ingresa a:");
+                a = reader.nextInt();
+                System.out.println("ingresa b");
+                b = reader.nextInt();
+                resta(a, b);
+                break;
 
-			case 3:
-				System.out.println("Ingresa a:");
-				a = reader.nextInt();
-				System.out.println("ingresa b");
-				b = reader.nextInt();
-				multiplicacion(a, b);
-				break;
+            case 3:
+                System.out.println("Ingresa a:");
+                a = reader.nextInt();
+                System.out.println("ingresa b");
+                b = reader.nextInt();
+                multiplicacion(a, b);
+                break;
 
-			case 4:
-				System.out.println("Ingresa a:");
-				a = reader.nextInt();
-				System.out.println("ingresa b");
-				b = reader.nextInt();
-				division(a, b);
-				break;
-			default:
-				System.out.println("Ingresa un valor valido");
-		}
+            case 4:
+                System.out.println("Ingresa a:");
+                a = reader.nextInt();
+                System.out.println("ingresa b");
+                b = reader.nextInt();
+                division(a, b);
+                break;
+            default:
+                System.out.println("Ingresa un valor valido");
+        }
 
-	}
+    }
 
-	public static int suma(int a, int b) {
-		int sum = 0;
-		sum = a + b;
-		System.out.println("El resultado es:" + sum);
-		return sum;
-	}
+    public static int suma(int a, int b) {
+        int sum = 0;
+        sum = a + b;
+        System.out.println("El resultado es:" + sum);
+        return sum;
+    }
 
-	private static long resta(int a, int b) {
-		int resta = 0;
-		resta = a - b;
-		System.out.println("El resultado es:" + resta);
-		return resta;
-	}
+    private static long resta(int a, int b) {
+        int resta = 0;
+        resta = a - b;
+        System.out.println("El resultado es:" + resta);
+        return resta;
+    }
 
-	private static long multiplicacion(int a, int b) {
-		int multiplicacion = a * b;
-		System.out.println("El resultado es:" + multiplicacion);
-		return multiplicacion;
-	}
+    private static long multiplicacion(int a, int b) {
+        int multiplicacion = a * b;
+        System.out.println("El resultado es:" + multiplicacion);
+        return multiplicacion;
+    }
 
-	private static int division(int a, int b) {
-		int division = a / b;
-		System.out.println("El resultado es:" + division);
-		return division;
-	}
+    private static int division(int a, int b) {
+        int division = a / b;
+        System.out.println("El resultado es:" + division);
+        return division;
+    }
 }
--- PruebaSet.java
+++ PruebaSet.java
@@ -5,23 +5,23 @@ import java.util.Set;
 import java.util.Collection;
 
 public class PruebaSet {
-	private static final String colores[] = {"rojo", "blanco", "azul", "verde"};
+    private static final String colores[] = {"rojo", "blanco", "azul", "verde"};
 
-	public PruebaSet() {
-		List<String> lista = Arrays.asList(colores);
-		System.out.printf("ArrayList: %s \n", lista);
-		imprimirSinDuplicados(lista);
-	}
+    public PruebaSet() {
+        List<String> lista = Arrays.asList(colores);
+        System.out.printf("ArrayList: %s \n", lista);
+        imprimirSinDuplicados(lista);
+    }
 
-	private void imprimirSinDuplicados(Collection<String> coleccion) {
-		Set<String> conjunto = new HashSet<String>(coleccion);
-		System.out.println("\nLos valores sin duplicados son: ");
-		for (String s: conjunto) {
-			System.out.printf(" %s ", s);
-		}
-		System.out.println();
-	}
-	public static void main (String[] args) {
-		new PruebaSet();
-	}
+    private void imprimirSinDuplicados(Collection<String> coleccion) {
+        Set<String> conjunto = new HashSet<String>(coleccion);
+        System.out.println("\nLos valores sin duplicados son: ");
+        for (String s: conjunto) {
+            System.out.printf(" %s ", s);
+        }
+        System.out.println();
+    }
+    public static void main (String[] args) {
+        new PruebaSet();
+    }
 }
